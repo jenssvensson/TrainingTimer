988cade9bfddc22d41039bf3a7fad5a459dd6e46
Jens Svensson <jens.fredrik.svensson@gmail.com>
2017-11-16 06:34:48 +0100

    Adding flex-layout

--- src/app/common/angular-material/angular-material.module.ts
+++ src/app/common/angular-material/angular-material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-
+import { MatButtonModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule]
+  imports: [MatButtonModule, FlexLayoutModule, MatToolbarModule],
+  exports: [MatButtonModule, FlexLayoutModule, MatToolbarModule]
 })
 export class AngularMaterialModule { }
